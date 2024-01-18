5c60302ade337dc478b5de1a6b7edb3f710eea6c
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-01-19 09:31:57 +1100

    QR code

--- U3A/wwwroot/js/qr.js
+++ U3A/wwwroot/js/qr.js
@@ -7,3 +7,14 @@
             height: 150
         });
 });
+
+function setQRCode() {
+    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+    document.getElementById("qrCode").innerText = '';
+    new QRCode(document.getElementById("qrCode"),
+        {
+            text: uri,
+            width: 150,
+            height: 150
+        });
+}
