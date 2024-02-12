69b41e5fdfa8ac6628d035e5dad20f07bbc724ad
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-02-12 16:09:56 +1100

    fix qr error

--- U3A/wwwroot/js/qr.js
+++ U3A/wwwroot/js/qr.js
@@ -1,20 +1,26 @@
 ﻿window.addEventListener("load", () => {
-    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
-    new QRCode(document.getElementById("qrCode"),
-        {
-            text: uri,
-            width: 150,
-            height: 150
-        });
+    var element = document.getElementById("qrCodeData");
+    if (element != null) {
+        const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+        new QRCode(document.getElementById("qrCode"),
+            {
+                text: uri,
+                width: 150,
+                height: 150
+            });
+    }
 });
 
 function setQRCode() {
-    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
-    document.getElementById("qrCode").innerText = '';
-    new QRCode(document.getElementById("qrCode"),
-        {
-            text: uri,
-            width: 150,
-            height: 150
-        });
+    var element = document.getElementById("qrCodeData");
+    if (element != null) {
+        const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+        document.getElementById("qrCode").innerText = '';
+        new QRCode(document.getElementById("qrCode"),
+            {
+                text: uri,
+                width: 150,
+                height: 150
+            });
+    }
 }
