1d59e6c63bef5da2e4303fc9ebb81ab0ff35a254
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-02-19 12:01:35 +1100

    display pdf as xtraReport

--- U3A.UI.Reports/PDFReport.Designer.cs
+++ U3A.UI.Reports/PDFReport.Designer.cs
@@ -31,19 +31,31 @@
             this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
             this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
             this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.PdfContent = new DevExpress.XtraReports.UI.XRPdfContent();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
             // TopMargin
             // 
+            this.TopMargin.HeightF = 0F;
             this.TopMargin.Name = "TopMargin";
             // 
             // BottomMargin
             // 
+            this.BottomMargin.HeightF = 0F;
             this.BottomMargin.Name = "BottomMargin";
             // 
             // Detail
             // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.PdfContent});
+            this.Detail.HeightF = 23F;
             this.Detail.Name = "Detail";
+            // 
+            // PdfContent
+            // 
+            this.PdfContent.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+            this.PdfContent.Name = "PdfContent";
+            this.PdfContent.SizeF = new System.Drawing.SizeF(827F, 23F);
             // 
             // PDFReport
             // 
@@ -52,6 +64,10 @@
             this.BottomMargin,
             this.Detail});
             this.Font = new DevExpress.Drawing.DXFont("Arial", 9.75F);
+            this.Margins = new DevExpress.Drawing.DXMargins(0F, 0F, 0F, 0F);
+            this.PageHeight = 1169;
+            this.PageWidth = 827;
+            this.PaperKind = DevExpress.Drawing.Printing.DXPaperKind.A4;
             this.Version = "23.2";
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
@@ -62,5 +78,6 @@
         private DevExpress.XtraReports.UI.TopMarginBand TopMargin;
         private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
         private DevExpress.XtraReports.UI.DetailBand Detail;
+        public DevExpress.XtraReports.UI.XRPdfContent PdfContent;
     }
 }
