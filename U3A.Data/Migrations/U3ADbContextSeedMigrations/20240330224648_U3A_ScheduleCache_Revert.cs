c320d9dbeb5702d420022bd698dc20f122dc3afe
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-03-31 09:55:54 +1100

    revert schedule

---
+++ U3A.Data/Migrations/U3ADbContextSeedMigrations/20240330224648_U3A_ScheduleCache_Revert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace U3A.Database.Migrations.U3ADbContextSeedMigrations
+{
+    /// <inheritdoc />
+    public partial class U3A_ScheduleCache_Revert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleCache");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleCache",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    jsonClasses = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    jsonEnrolments = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleCache", x => x.ID);
+                });
+        }
+    }
+}
