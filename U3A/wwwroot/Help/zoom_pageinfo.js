c2970cd0fb6ed62386e6db59c5c14971e0cdb984
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-06-22 12:36:28 +1000

    help doc update

--- U3A/wwwroot/Help/zoom_pageinfo.js
+++ U3A/wwwroot/Help/zoom_pageinfo.js
@@ -66,7 +66,7 @@ pageinfo = [[0,0,0,0,null],
 	[0,0,0,0,null],
 	[0,0,0,0,null]];
 pagedata = [ ["./2023-version-history.html","2023 Version History","version 2023.12 Add/Edit Receipts Added the ability to set/reset the participant&#39;s Financial Year. Please refer to Add/Edit Receipts for details. ...",""],
-["./2024-version-history.html","2024 Version History","Version 2024.Current Add/Edit Terms Change to business rules for terms... The enrolment dates for terms may not overlap with enrolment dates for t...",""],
+["./2024-version-history.html","2024 Version History","Version 2024.Current DevExpress 24.1 Upgrade Our Developer Express component software has been upgraded to 24.1.3. Any screen corruption issue wil...",""],
 ["./add_edit-committee.html","Participation Menu &#62; Add/Edit Committee","This procedure allows you to assign a Committee position to a participant.",""],
 ["./add_edit-course-types.html","Course Menu &#62; Add/Edit Course Types","A Course Type categorizes Courses &#38; Activities into groups. Common Course Types might include...",""],
 ["./add_edit-courses.html","Course Menu &#62; Add/Edit Courses","A Course is an activity organised by the U3A group. ",""],
