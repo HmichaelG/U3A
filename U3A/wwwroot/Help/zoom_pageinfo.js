ed46de4b8f4f52dde1fa375bb48564952acbd290
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-07-04 15:44:27 +1000

    help doc updates

--- U3A/wwwroot/Help/zoom_pageinfo.js
+++ U3A/wwwroot/Help/zoom_pageinfo.js
@@ -66,7 +66,7 @@ pageinfo = [[0,0,0,0,null],
 	[0,0,0,0,null],
 	[0,0,0,0,null]];
 pagedata = [ ["./2023-version-history.html","2023 Version History","version 2023.12 Add/Edit Receipts Added the ability to set/reset the participant&#39;s Financial Year. Please refer to Add/Edit Receipts for details. ...",""],
-["./2024-version-history.html","2024 Version History","Version 2024.7.2 Help Document: Add/Edit Courses Added note to class Recurrence field to advise that classes with a non-null recurrence value shou...",""],
+["./2024-version-history.html","2024 Version History","Version 2024.7.4 Organisational Details The field, UTCOffset has been replaced with Timezone. All U3A databases have been configured with the defa...",""],
 ["./add_edit-committee.html","Participation Menu &#62; Add/Edit Committee","This procedure allows you to assign a Committee position to a participant.",""],
 ["./add_edit-course-types.html","Course Menu &#62; Add/Edit Course Types","A Course Type categorizes Courses &#38; Activities into groups. Common Course Types might include...",""],
 ["./add_edit-courses.html","Course Menu &#62; Add/Edit Courses","A Course is an activity organised by the U3A group. ",""],
