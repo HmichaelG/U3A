80b2440567e80496356353fd993846b8100b9e45
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-08-05 19:00:20 +1000

    ensure host is not null

--- U3A.Services/ErrorBoundaryLoggingService.cs
+++ U3A.Services/ErrorBoundaryLoggingService.cs
@@ -13,31 +13,38 @@ using System.Threading.Tasks;
 using U3A.Database;
 using U3A.Model;
 
-namespace U3A.Services
-{
-    public class ErrorBoundaryLoggingService : IErrorBoundaryLogger
-    {
-        private readonly IHttpContextAccessor httpContextAccessor;
+namespace U3A.Services;
 
-        public ErrorBoundaryLoggingService(IHttpContextAccessor httpContextAccessor)
+public class ErrorBoundaryLoggingService : IErrorBoundaryLogger
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public ErrorBoundaryLoggingService(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+    public async ValueTask LogErrorAsync(Exception exception)
+    {
+        //Ignore if we do not have context
+        if (httpContextAccessor?.HttpContext != null)
         {
-            this.httpContextAccessor = httpContextAccessor;
-        }
-        public async ValueTask LogErrorAsync(Exception exception)
-        {
-            using (LogContext.PushProperty("LogEvent", "Unhandled Exception"))
+            if (!string.IsNullOrWhiteSpace(httpContextAccessor?.HttpContext?.Request?.Host.Host))
             {
-                using (LogContext.PushProperty("Tenant",
-                            httpContextAccessor?.HttpContext?.Request?.Host.Host))
+                using (LogContext.PushProperty("LogEvent", "Unhandled Exception"))
                 {
-                    using (LogContext.PushProperty("User",
-                            httpContextAccessor?.HttpContext.User.Identity.Name))
+                    using (LogContext.PushProperty("Tenant",
+                                httpContextAccessor?.HttpContext?.Request?.Host.Host))
                     {
-                        Log.Error(exception, "{p0}", exception.Message);
+                        using (LogContext.PushProperty("User",
+                                httpContextAccessor?.HttpContext.User.Identity.Name))
+                        {
+                            Log.Error(exception, "{p0}", exception.Message);
+                        }
                     }
                 }
             }
-            return;
         }
     }
+
 }
+
