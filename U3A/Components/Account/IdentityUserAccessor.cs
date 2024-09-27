43d589e558cf25b8b5af306fd6fda90b00e8ffe9
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-09-27 15:00:40 +1000

    fix warnings

--- U3A/Components/Account/IdentityUserAccessor.cs
+++ U3A/Components/Account/IdentityUserAccessor.cs
@@ -14,7 +14,9 @@ namespace U3A.Components.Account
                 redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
             }
 
+#pragma warning disable CS8603 // Possible null reference return.
             return user;
+#pragma warning restore CS8603 // Possible null reference return.
         }
     }
 }
