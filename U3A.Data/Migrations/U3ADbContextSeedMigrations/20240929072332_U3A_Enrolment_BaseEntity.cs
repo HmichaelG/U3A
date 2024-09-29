5964e14c190face113491760df0f2bc051a0d243
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-09-29 17:35:48 +1000

    Enrolment : BaseEntity

---
+++ U3A.Data/Migrations/U3ADbContextSeedMigrations/20240929072332_U3A_Enrolment_BaseEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace U3A.Database.Migrations.U3ADbContextSeedMigrations
+{
+    /// <inheritdoc />
+    public partial class U3A_Enrolment_BaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Enrolment",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Enrolment",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "Enrolment",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Enrolment");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Enrolment");
+
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "Enrolment");
+        }
+    }
+}
