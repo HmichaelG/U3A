baee69e7125caaf747b0399608f13b0ca600e646
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-10-04 12:35:03 +1000

    UI Refresh

--- U3A.UI.Reports/ClassScheduleRpt.Designer.cs
+++ U3A.UI.Reports/ClassScheduleRpt.Designer.cs
@@ -509,7 +509,7 @@
             this.xrRichText1.Borders = DevExpress.XtraPrinting.BorderSide.None;
             this.xrRichText1.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
             new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Html", "[Course].[DescriptionForXrRichText]")});
-            this.xrRichText1.Font = new DevExpress.Drawing.DXFont("Arial", 10F);
+            this.xrRichText1.Font = new DevExpress.Drawing.DXFont("Arial", 6F);
             this.xrRichText1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
             this.xrRichText1.Name = "xrRichText1";
             this.xrRichText1.Padding = new DevExpress.XtraPrinting.PaddingInfo(8, 8, 8, 8, 100F);
