abdb75edf95ff39b4acfdfdc69e568a9b7f9ec8d
HmichaelG <46980223+HmichaelG@users.noreply.github.com>
2024-10-29 07:21:38 +1100

    help doc update

---
+++ U3A.Data/Migrations/U3ADbContextSeedMigrations/20241027234403_U3A_Person_Remove_Unused_Index.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace U3A.Database.Migrations.U3ADbContextSeedMigrations
+{
+    /// <inheritdoc />
+    public partial class U3A_Person_Remove_Unused_Index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Person_ConversionID",
+                table: "Person");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Person_DataImportTimestamp",
+                table: "Person");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Person_LastName_FirstName_Email",
+                table: "Person");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Person_PersonID",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DataImportTimestamp",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DataImportTimestamp",
+                table: "Person",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_ConversionID",
+                table: "Person",
+                column: "ConversionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_DataImportTimestamp",
+                table: "Person",
+                column: "DataImportTimestamp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_LastName_FirstName_Email",
+                table: "Person",
+                columns: new[] { "LastName", "FirstName", "Email" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_PersonID",
+                table: "Person",
+                column: "PersonID");
+        }
+    }
+}
