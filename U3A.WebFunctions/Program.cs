b94ceb080def76148c9ef4445d238bf25f6b1561
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2024-11-17 17:08:46 +1100

    update for .net 9

--- U3A.WebFunctions/Program.cs
+++ U3A.WebFunctions/Program.cs
@@ -1,12 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Core;
-using Serilog.Events;
-using Serilog.Exceptions;
-using Serilog.Filters;
-using Serilog.Formatting.Json;
-using SeriSQL = Serilog.Sinks.MSSqlServer;
 using static DevExpress.Data.Utils.AnnotationAttributes;
 using System.Globalization;
 using System.Collections.ObjectModel;
@@ -14,25 +7,16 @@ using System.Data;
 using U3A.WebFunctions;
 using Microsoft.Extensions.Configuration;
 using DevExpress.Drawing;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Worker;
 
 
 DevExpress.Utils.DeserializationSettings.RegisterTrustedAssembly(typeof(U3A.UI.Reports.ProFormaReportFactory).Assembly);
 
-var builder = WebApplication.CreateBuilder(args);
-string? tenantConnectionString = builder.Configuration.GetConnectionString(Common.TENANT_CN_CONFIG);
+var builder = FunctionsApplication.CreateBuilder(args);
 
-var columnOptions = new SeriSQL.ColumnOptions
-{
-    AdditionalColumns = new Collection<SeriSQL.SqlColumn>
-    {
-        new SeriSQL.SqlColumn
-                { ColumnName = "Tenant", PropertyName = "Tenant", DataType = SqlDbType.NVarChar, DataLength = 64 },
-        new SeriSQL.SqlColumn
-                { ColumnName = "User", PropertyName = "User", DataType = SqlDbType.NVarChar, DataLength = 64 },
-        new SeriSQL.SqlColumn
-                { ColumnName = "LogEvent", PropertyName = "LogEvent", DataType = SqlDbType.NVarChar, DataLength = 64 },
-    }
-};
+string? tenantConnectionString = builder.Configuration.GetConnectionString(Common.TENANT_CN_CONFIG);
 
 DevExpress.Drawing.Internal.DXDrawingEngine.ForceSkia();
 foreach (var file in Directory.GetFiles(@"fonts"))
@@ -41,28 +25,11 @@ foreach (var file in Directory.GetFiles(@"fonts"))
 }
 
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-     .ConfigureLogging((hostingContext, logging) =>
-     {
-         Log.Logger = new LoggerConfiguration()
-             .Enrich.FromLogContext()
-             .Enrich.WithExceptionDetails()
-             .WriteTo.Logger(lc => lc
-                 .Filter.ByIncludingOnly(Matching.WithProperty("AutoEnrolParticipants"))
-                 .WriteTo.MSSqlServer(connectionString: tenantConnectionString,
-                                         formatProvider: new CultureInfo("en-AU"),                                         
-                                         sinkOptions: new SeriSQL.MSSqlServerSinkOptions
-                                         {
-                                             TableName = "LogAutoEnrol"
-                                         },
-                                             columnOptions: columnOptions
-                                         )
-                 )
-             .CreateLogger();
+builder.ConfigureFunctionsWebApplication();
 
-         logging.AddSerilog(Log.Logger, true);
-     })
-    .Build();
-host.Run();
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
 
+builder.Build().Run();
