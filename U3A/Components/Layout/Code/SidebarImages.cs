bea2c9e215d39e7cc43113a4b2b1006d7138df75
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2024-12-27 16:53:26 +1100

    Refactor sidebar and toolbar components
    Updated AdminMemberPortal.razor to simplify toolbar items by removing the RenderStyleMode attribute. Replaced SidebarImage with SidebarImageComponent in SideBar.razor. Removed media queries from SideBar.razor.css, affecting responsive design. Deleted SidebarImage.razor and its seasonal image logic. Updated U3A.csproj to exclude sidebar-vectors.svg from the publish directory. Removed weather.svg. Enhanced SidebarImageComponent.razor with new services and conditional logic for Christmas-themed images. Added media queries to SidebarImageComponent.razor.css for responsive styling. Introduced SidebarImages.cs to manage sidebar images.

---
+++ U3A/Components/Layout/Code/SidebarImages.cs
@@ -0,0 +1,18 @@
+﻿namespace U3A;
+
+public class SidebarImages : List<SidebarImage>
+{
+    public SidebarImages()
+    {
+        Add(new SidebarImage { MenuName = "Monochrome Flower", Filename = "flower-light.svg" });
+        Add(new SidebarImage { MenuName = "Colored Flower", Filename = "flower-color.svg" });
+    }
+}
+
+public class SidebarImage
+{
+    public string MenuName { get; set; }
+    public string Filename { get; set; }
+    public string CssClass { get; set; } = "sidebar-image";
+
+}
