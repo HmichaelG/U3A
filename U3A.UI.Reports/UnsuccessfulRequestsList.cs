c659a7f7c1fde92057ea76a7e48178f5b6ab084b
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-01-01 08:40:47 +1100

    Add 2025 Version History and Unsuccessful Requests Report
    Updated table_of_contents.xml to include a new topic reference for the "2025 Version History". Created 2025-Version-History.xml with details about the new "Unsuccessful Requests List" report. Updated U3A.UI.Reports.csproj to include UnsuccessfulRequestsList.cs as an XtraReport. Added UnsuccessfulRequestsList class and resource file. Updated MainMenu.razor to include a new menu item for the report. Changed project version in U3A.csproj to 2025.1.1.0000. Updated 2024-version-history.html to link to the new 2025-version-history.html. Created 2025-version-history.html to document changes for 2025. Updated settings.js with new counts for keywords, variants, and pages. Added new entries to pageinfo and pagedata arrays in zoom_pageinfo.js for the 2025 Version History page.

---
+++ U3A.UI.Reports/UnsuccessfulRequestsList.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using U3A.BusinessRules;
+using U3A.Database;
+using U3A.Model;
+
+namespace U3A.UI.Reports
+{
+    public partial class UnsuccessfulRequestsList : DevExpress.XtraReports.UI.XtraReport, IXtraReportWithDbContext
+    {
+        public UnsuccessfulRequestsList()
+        {
+            InitializeComponent();
+            prmFinancialStatus.Value = null;
+        }
+
+        public U3ADbContext DbContext { get; set; }
+
+        private void CourseByParticipantList_ParametersRequestBeforeShow(object sender, DevExpress.XtraReports.Parameters.ParametersRequestEventArgs e)
+        {
+            var terms = BusinessRule.SelectableTerms(DbContext);
+            objectDataSource2.DataSource = terms;
+            var term = BusinessRule.CurrentTerm(DbContext);
+            paramTermSummary.Value = term?.TermSummary;
+            paramTerm.Value = term?.ID;
+            objectDataSource3.DataSource = BusinessRule.SelectableCourses(DbContext, term);
+        }
+
+        private void CourseByParticipantList_ParametersRequestValueChanged(object sender, DevExpress.XtraReports.Parameters.ParametersRequestValueChangedEventArgs e)
+        {
+            if (e.ChangedParameterInfo.Parameter == paramTerm)
+            {
+                var term = DbContext.Term.Find((Guid)paramTerm.Value);
+                objectDataSource3.DataSource = BusinessRule.SelectableCourses(DbContext, term);
+            }
+        }
+        private void CourseByParticipantList_ParametersRequestSubmit(object sender, DevExpress.XtraReports.Parameters.ParametersRequestEventArgs e)
+        {
+            var term = DbContext.Term.Find((Guid)paramTerm.Value);
+            paramTermSummary.Value = term?.TermSummary;
+            DataSource = GetData(term);
+        }
+
+        List<Person> GetData(Term term)
+        {
+            var result = BusinessRule.SelectablePersonsWithEnrolments(DbContext, term.ID, null);
+            result = result.Where(x => !x.Enrolments.Any(c => !c.IsWaitlisted)).ToList();
+            if (prmFinancialStatus.Value == null) { return result; }
+            switch ((int)prmFinancialStatus.Value)
+            {
+                case 0:
+                    //Financial
+                    result = result.Where(x => x.FinancialTo >= term.Year).ToList();
+                    break;
+                case 1:
+                    //Unfinancial
+                    result = result.Where(x => x.FinancialTo < term.Year).ToList();
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
