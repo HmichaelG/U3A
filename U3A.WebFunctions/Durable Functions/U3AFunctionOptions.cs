72884aba487f604e7079d664f368b5ab1eea6dc4
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-01-31 16:10:13 +1100

    Offload correspondance function to azure function

--- U3A.WebFunctions/Durable Functions/U3AFunctionOptions.cs
+++ U3A.WebFunctions/Durable Functions/U3AFunctionOptions.cs
@@ -8,8 +8,7 @@ public class U3AFunctionOptions
     public U3AFunctionOptions() { }
     public U3AFunctionOptions(HttpRequestData req)
     {
-        var queryStrings = req.Query.GetValues("tenant");
-        if (queryStrings?.Count() > 0)
+        if (req.Query.TryGetValue("tenant", out var tenantValues) && tenantValues.Count > 0)
         {
             TenantIdentifier = queryStrings[0];
         }
