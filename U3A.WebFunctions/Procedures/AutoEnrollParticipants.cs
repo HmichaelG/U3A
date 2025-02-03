cd4bc046636673492b1b5cca0eb38a78ef7c7143
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-02-04 07:35:48 +1100

    revert to previous version

--- U3A.WebFunctions/Procedures/AutoEnrollParticipants.cs
+++ U3A.WebFunctions/Procedures/AutoEnrollParticipants.cs
@@ -10,7 +10,6 @@ namespace U3A.WebFunctions.Procedures
     public static class AutoEnrollParticipants
     {
         public static async Task<bool> Process(TenantInfo tenant,
-                                            U3AFunctionOptions options,
                                             string tenantConnectionString,
                                             ILogger logger)
         {
