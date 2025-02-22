926ae1fe8bcd7935c51e6fa8da57d5c0f9585908
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-02-22 15:11:11 +1100

    rework AiChat

---
+++ U3A.Model/Projections/SchedulledClass.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace U3A.Model
+{
+
+    public class ScheduledClass
+    {
+        public Guid ID { get; set; }
+
+        // From Course
+
+        public string CourseName { get; set; }
+        public bool IsFeaturedCourse { get; set; }
+        public string CourseParticipationType { get; set; }
+        public bool EnforceOneClassPerStudent { get; set; }
+        public decimal CourseFeePerYear { get; set; }
+        public string? CourseFeePerYearDescription { get; set; }
+        public decimal CourseFeePerTerm { get; set; }
+        public string? CourseFeePerTermDescription { get; set; }
+        public decimal Duration { get; set; }
+        public int RequiredStudents { get; set; }
+        public int MaximumStudents { get; set; }
+        public bool AllowAutoEnroll { get; set; } = true;
+        public string CourseType { get; set; }
+        public string? OfferedBy { get; set; } // The U3A that owns the course
+
+        // From Class
+
+        public Boolean OfferedTerm1 { get; set; } 
+        public Boolean OfferedTerm2 { get; set; } 
+        public Boolean OfferedTerm3 { get; set; } 
+        public Boolean OfferedTerm4 { get; set; } 
+        public DateOnly? StartDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly? EndTime { get; set; }
+        public string Occurrence { get; set; }
+        public int? Recurrence { get; set; }
+        public string OnDay { get; set; }
+        public string OccurrenceTextBrief { get; set; }
+        public string OccurrenceText { get; set; }
+        public string Venue { get; set; }
+        public string VenueAddress { get; set; }
+        public List<ScheduledPerson> Leaders { get; set; } = new ();
+        public List<ScheduledPerson> Clerks { get; set; } = new ();
+        public int TotalActiveStudents { get; set; }
+        public int TotalWaitlistedStudents { get; set; }
+        public double ParticipationRate { get; set; }
+        public List<DateTime> ClassDates { get; set; } = new();
+
+    }
+
+    public class ScheduledPerson
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public bool IsGuestLeader { get; set; } = false;
+    }
+    public class AIChatClassData
+    {
+        public List<ScheduledClass> Classes { get; set; }
+        public List<Term> Terms { get; set; }
+    }
+
+}
---
+++ U3A.Services/Business Rules/AIClasssChatRules.cs
@@ -0,0 +1,177 @@
+﻿using DevExpress.Blazor;
+using DevExpress.XtraRichEdit.Commands.Internal;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using U3A.Database;
+using U3A.Model;
+using U3A.Services;
+using static Azure.Core.HttpHeader;
+
+namespace U3A.BusinessRules
+{
+    public static partial class BusinessRule
+    {
+        public static async Task<AIChatClassData> GetAIChatClassDataAsync(U3ADbContext dbc,
+                                                        TenantDbContext dbcT,
+                                                        TenantInfoService tenantService)
+        {
+            var data = new AIChatClassData();
+            // settings
+            var settings = await dbc.SystemSettings.OrderBy(x => x.ID).FirstOrDefaultAsync();
+
+            // Terms
+            data.Terms = await BusinessRule.SelectableTermsInCurrentYearAsync(dbc);
+
+            // Classes
+            var term = await BusinessRule.CurrentEnrolmentTermAsync(dbc);
+            if (term == null)
+            {
+                var currentTerm = await BusinessRule.CurrentTermAsync(dbc);
+            }
+            if (term != null)
+            {
+
+                var prevTerm = await BusinessRule.GetPreviousTermAsync(dbc, term.Year, term.TermNumber) ?? term;
+
+                // Fast lookup from Schedule cache
+                var classes = await BusinessRule.RestoreClassesFromScheduleAsync(dbc, dbcT, tenantService, term, settings, true, true);
+               // classes = classes.Take(102).ToList();
+                await getAppointmentsForAiClass(dbc, term, classes);
+                // Transpose to ScheduledClasses
+                data.Classes = classes.Select(x => new ScheduledClass()
+                {
+                    ID = x.ID,
+
+                    // From Course
+
+                    CourseName = x.Course.Name,
+                    CourseParticipationType = (x.Course.CourseParticipationTypeID == 0)
+                                                ? "Same students in all classes"
+                                                : "Different students in each class",
+                    IsFeaturedCourse = x.Course.IsFeaturedCourse,
+                    EnforceOneClassPerStudent = x.Course.EnforceOneStudentPerClass,
+                    CourseFeePerYear = x.Course.CourseFeePerYear,
+                    CourseFeePerYearDescription = x.Course.CourseFeePerYearDescription,
+                    CourseFeePerTerm = x.Course.CourseFeePerTerm,
+                    CourseFeePerTermDescription = x.Course.CourseFeePerTermDescription,
+                    Duration = x.Course.Duration,
+                    RequiredStudents = x.Course.RequiredStudents,
+                    MaximumStudents = x.Course.MaximumStudents,
+                    AllowAutoEnroll = x.Course.AllowAutoEnrol,
+                    CourseType = x.Course.CourseType.Name,
+                    OfferedBy = x.Course.OfferedBy,
+
+                    // From Class
+
+                    OfferedTerm1 = x.OfferedTerm1,
+                    OfferedTerm2 = x.OfferedTerm2,
+                    OfferedTerm3 = x.OfferedTerm3,
+                    OfferedTerm4 = x.OfferedTerm4,
+                    StartDate = (x.StartDate != null)
+                                    ? DateOnly.FromDateTime(x.StartDate.Value)
+                                    : null,
+                    StartTime = TimeOnly.FromDateTime(x.StartTime),
+                    EndTime = (x.EndTime != null) ? TimeOnly.FromDateTime(x.EndTime.Value)
+                                                  : null,
+                    Occurrence = x.Occurrence.Name,
+                    Recurrence = x.Recurrence,
+                    OnDay = x.OnDay.Day,
+                    OccurrenceTextBrief = x.OccurrenceTextBrief,
+                    OccurrenceText = x.OccurrenceText,
+                    Venue = x.Venue.Name,
+                    VenueAddress = x.Venue.Address,
+                    TotalActiveStudents = x.TotalActiveStudents,
+                    TotalWaitlistedStudents = x.TotalWaitlistedStudents,
+                    ParticipationRate = x.ParticipationRate
+
+                }).ToList();
+                classes.ForEach(delegate (Class c)
+                {
+                    var sc = data.Classes.Find(x => x.ID == c.ID);
+                    if (sc != null)
+                    {
+                        if (!string.IsNullOrWhiteSpace(c.GuestLeader)) sc.Leaders.Add(new ScheduledPerson()
+                        {
+                            Name = c.GuestLeader,
+                            IsGuestLeader = true
+                        });
+                        if (c.Leader != null) sc.Leaders.Add(new ScheduledPerson()
+                        {
+                            Name = c.Leader.FullNameWithPostNominals,
+                            Email = c.Leader.Email,
+                            Phone = c.Leader.AdjustedHomePhone,
+                            Mobile = c.Leader.AdjustedMobile
+                        });
+                        if (c.Leader2 != null) sc.Leaders.Add(new ScheduledPerson()
+                        {
+                            Name = c.Leader2.FullNameWithPostNominals,
+                            Email = c.Leader2.Email,
+                            Phone = c.Leader2.AdjustedHomePhone,
+                            Mobile = c.Leader2.AdjustedMobile
+                        });
+                        if (c.Leader3 != null) sc.Leaders.Add(new ScheduledPerson()
+                        {
+                            Name = c.Leader3.FullNameWithPostNominals,
+                            Email = c.Leader3.Email,
+                            Phone = c.Leader3.AdjustedHomePhone,
+                            Mobile = c.Leader3.AdjustedMobile
+                        });
+                        foreach (var clerk in c.Clerks)
+                        {
+                            sc.Clerks.Add(new ScheduledPerson()
+                            {
+                                Name = clerk.FullNameWithPostNominals,
+                                Email = clerk.Email,
+                                Phone = clerk.AdjustedHomePhone,
+                                Mobile = clerk.AdjustedMobile
+                            });
+                        }
+                        foreach (var d in c.ClassDates)
+                        {
+                            sc.ClassDates.Add(d);
+                        }
+                        if (sc.StartDate == null)
+                        {
+                            sc.StartDate = DateOnly.FromDateTime(sc.ClassDates.OrderBy(x => x).FirstOrDefault());
+                        }
+                    }
+                });
+            }
+            return data;
+        }
+
+        static async Task getAppointmentsForAiClass(U3ADbContext dbc, Term term,
+            IEnumerable<Class> classes)
+        {
+            var dataStorage = await BusinessRule.GetCalendarDataStorageAsync(dbc, term);
+            var range = new DxSchedulerDateTimeRange(term.StartDate, new DateTime(term.Year, 12, 31));
+            Dictionary<Guid, List<DxSchedulerAppointmentItem>> classAppointments = new();
+            foreach (var a in dataStorage?.GetAppointments(range))
+            {
+                Class c = (Class)a.CustomFields["Source"];
+                if (c != null && (int)a.LabelId != 9)
+                {
+                    if (!classAppointments.ContainsKey(c.ID))
+                    {
+                        classAppointments.Add(c.ID, new List<DxSchedulerAppointmentItem>());
+                    }
+                    classAppointments[c.ID].Add(a);
+                }
+            }
+            foreach (Class c in classes)
+            {
+                if (classAppointments.ContainsKey(c.ID))
+                {
+                    foreach (var a in classAppointments[c.ID])
+                    {
+                        c.ClassDates.Add(a.Start);
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
