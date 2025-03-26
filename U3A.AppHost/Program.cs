771f28ecc715bf9ad30cc6914754d4a6c4747723
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-03-26 11:11:27 +1100

    Dx 24.2.6

--- U3A.AppHost/Program.cs
+++ U3A.AppHost/Program.cs
@@ -4,9 +4,15 @@ using Serilog.Events;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var openai = builder.ExecutionContext.IsPublishMode
+    ? builder.AddAzureOpenAI("openai")
+    : builder.AddConnectionString("openai");
+
 var api = builder.AddAzureFunctionsProject<Projects.U3A_WebFunctions>("u3a-webfunctions");
 
-var mainApp = builder.AddProject<Projects.U3A>("u3a").WaitFor(api);
+var mainApp = builder.AddProject<Projects.U3A>("u3a")
+    .WithReference(openai)
+    .WaitFor(api);
 
 mainApp.WithEnvironment("aspire-webfunctions-url", api.GetEndpoint("http"));
 
