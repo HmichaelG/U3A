d7425c227c782b7a3cf9f0a07217588a5f7adbc1
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-04-25 12:36:04 +1000

    UI refresh

--- U3A/Extensions/HostBuilder/DevExpressServiceExtensions.cs
+++ U3A/Extensions/HostBuilder/DevExpressServiceExtensions.cs
@@ -11,7 +11,7 @@ public static class DevExpressServiceExtensions
     {
         DevExpress.Utils.DeserializationSettings.RegisterTrustedAssembly(typeof(U3A.UI.Reports.ProFormaReportFactory).Assembly);
         DevExpress.Utils.DeserializationSettings.RegisterTrustedAssembly(typeof(U3A.Model.Class).Assembly);
-        DevExpress.Drawing.Settings.DrawingEngine = DrawingEngine.Default;
+        DevExpress.Drawing.Settings.DrawingEngine = DrawingEngine.Skia;
         foreach (var file in Directory.GetFiles(@"wwwroot/fonts"))
         {
             DXFontRepository.Instance.AddFont(file);
