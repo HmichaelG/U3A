908f6b2b3a517917696fa75f791b3c6ff8ecc71f
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-06-05 14:11:54 +1000

    LMD changes

---
+++ U3A.Data/Migrations/U3ADbContextSeedMigrations/20250605033933_U3A_LuckyMemberDraw_BaseEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace U3A.Database.Migrations.U3ADbContextSeedMigrations
+{
+    /// <inheritdoc />
+    public partial class U3A_LuckyMemberDraw_BaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "LuckyMemberDraw",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "LuckyMemberDraw",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "LuckyMemberDraw",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "LuckyMemberDraw");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "LuckyMemberDraw");
+
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "LuckyMemberDraw");
+        }
+    }
+}
--- U3A.Model/LuckyMemberDraw.cs
+++ U3A.Model/LuckyMemberDraw.cs
@@ -5,16 +5,26 @@ using System.Runtime.CompilerServices;
 
 namespace U3A.Model
 {
-    public class LuckyMemberDraw 
+    public class LuckyMemberDraw : BaseEntity
     {
         public Guid ID { get; set; }
         [Required]
         public string Name { get; set; } = "Lucky Member Draw";
 
         public bool IsComplete {get; set; }
-
-        [Timestamp]
-        public byte[] Version { get; set; }
+        
+        [NotMapped]
+        public string Secret { 
+            get {
+                string result = string.Empty;
+                if (CreatedOn.HasValue)
+                {
+                    var value = CreatedOn.Value.Millisecond * 1000 + CreatedOn.Value.Nanosecond;
+                    result = $"{value.ToString("0000000")}";
+                }
+                return result;
+            } 
+        }
 
         //Navigation properties
         public List<LuckyMemberDrawEntrant> LuckyMemberDrawEntrants { get; set; } = new List<LuckyMemberDrawEntrant>();
