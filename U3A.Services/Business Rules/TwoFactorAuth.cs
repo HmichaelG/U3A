e8deed73e4568b79e1d359ab6ddcab8e1daad591
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-06-17 14:02:33 +1000

    correct update issue

--- U3A.Services/Business Rules/TwoFactorAuth.cs
+++ U3A.Services/Business Rules/TwoFactorAuth.cs
@@ -20,6 +20,7 @@ public static partial class BusinessRule
         if (user.TwoFactorGracePeriodStart == null)
         {
             user.TwoFactorGracePeriodStart = DateOnly.FromDateTime(DateTime.UtcNow);
+            dbc.Update(user);
             await dbc.SaveChangesAsync();
         }
 
