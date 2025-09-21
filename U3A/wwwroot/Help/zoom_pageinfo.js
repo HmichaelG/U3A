fc4bcb355a6a6955487dad3e23186bb0e22726a3
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-09-22 09:58:45 +1000

    doc update

--- U3A/wwwroot/Help/zoom_pageinfo.js
+++ U3A/wwwroot/Help/zoom_pageinfo.js
@@ -82,7 +82,7 @@ pageinfo = [[0,0,0,0,null],
 	[0,0,0,0,null]];
 pagedata = [ ["./2023-version-history.html","2023 Version History","version 2023.12 Add/Edit Receipts Added the ability to set/reset the participant&#39;s Financial Year. Please refer to Add/Edit Receipts for details. ...",""],
 ["./2024-version-history.html","2024 Version History","Version 2024.12.30 Themes &#38; Settings You can now choose the image to be displayed at the bottom of the sidebar menu. The default is to display...",""],
-["./2025-version-history.html","2025 Version History","Version 2025.9.16 System DevExpress 25.1. 5 minor update. Member Portal &#124; Request Enrolments An issue that caused high CPU server usage has been r...",""],
+["./2025-version-history.html","2025 Version History","Version 2025.9.Current Organisation Details Added a new field, Ceased Reasons to enter reason a member may cease a membership. Add/Edit Participan...",""],
 ["./add_edit-committee.html","Participation Menu &#62; Add/Edit Committee","This procedure allows you to assign a Committee position to a participant.",""],
 ["./add_edit-contacts.html","Participation Menu &#62; Add/Edit Contacts","A contact can be any person associated with your U3A but is not a participant. Think of the list of contacts as your U3A&#39;s virtual Teledex.",""],
 ["./add_edit-course-types.html","Course Menu &#62; Add/Edit Course Types","A Course Type categorizes Courses &#38; Activities into groups. Common Course Types might include...",""],
