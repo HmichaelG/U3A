a9b734115fa55227965987adff73664bce88ded1
Michael Hanlon <46980223+HmichaelG@users.noreply.github.com>
2025-10-06 06:38:47 +1100

    mark obsolete member

--- U3A.Model/Course.cs
+++ U3A.Model/Course.cs
@@ -70,7 +70,7 @@ namespace U3A.Model
         public bool OverrideComplimentaryPerYearFee { get; set; }
         public bool LeadersPayYearFee { get; set; }
 
-        [Required]
+        [Obsolete][Required]
         [Precision(precision: 18, 2)]
         [DefaultValue(0.00)]
         [Comment("Optional fee per term)")]
